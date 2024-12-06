b6dd26a426fc9a2ff2b2dc4c92321ed9c578544c
Jeu14 <jedissonsantana34@gmail.com>
2024-12-06 16:07:42 -0300

    feat(Image): Add size and mimitype validation

--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma.service';
 import { env } from 'process';
 import { s3 } from 'src/providers/b2.service';
@@ -9,6 +9,17 @@ export class ImageService {
   constructor(private readonly prisma: PrismaService) {}
 
   async uploadFile(file: Express.Multer.File) {
+    const MAX_IMAGE_SIZE = 15 * 1024 * 1024;
+    const ALLOWED_IMAGE_TYPES = ['image/jpeg', 'image/png', 'image/gif'];
+
+    if (file.mimetype.startsWith('image/') && file.size > MAX_IMAGE_SIZE) {
+      throw new BadRequestException('Each image cannot exceed 15 MB.');
+    }
+
+    if (!ALLOWED_IMAGE_TYPES.includes(file.mimetype)) {
+      throw new BadRequestException('Only images are allowed.');
+    }
+
     const bucketName = env.BUCKET_NAME;
     const fileName = file.originalname;
     const uploadParams = {
