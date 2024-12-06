79e7022f8677fb575d0b52e88eae5f9b488f3386
Jeu14 <jedissonsantana34@gmail.com>
2024-12-06 17:09:51 -0300

    feat(Image):Add  user_id

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -1,15 +1,18 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Post, Request, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { ImageService } from './image.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('image')
 export class ImageController {
   constructor(private readonly imageService: ImageService) {}
 
+  @UseGuards(AuthGuard('jwt'))
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
-  async uploadImage(@UploadedFile() file: Express.Multer.File) {
-    const savedImage = await this.imageService.uploadFile(file);
+  async uploadImage(@Request() req, @UploadedFile() file: Express.Multer.File) {
+    const userId = req.user.userId;
+    const savedImage = await this.imageService.uploadFile(file, userId);
     return savedImage;
   }
 }
--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -3,25 +3,29 @@ import { PrismaService } from 'src/database/prisma.service';
 import { env } from 'process';
 import { s3 } from 'src/providers/b2.service';
 import { PutObjectCommand } from '@aws-sdk/client-s3';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class ImageService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async uploadFile(file: Express.Multer.File) {
+  async uploadFile(file: Express.Multer.File, user_id: string) {
+    if (!file) {
+      throw new BadRequestException('No file uploaded.');
+    }
+
     const MAX_IMAGE_SIZE = 15 * 1024 * 1024;
     const ALLOWED_IMAGE_TYPES = ['image/jpeg', 'image/png', 'image/gif'];
 
     if (file.mimetype.startsWith('image/') && file.size > MAX_IMAGE_SIZE) {
       throw new BadRequestException('Each image cannot exceed 15 MB.');
     }
-
     if (!ALLOWED_IMAGE_TYPES.includes(file.mimetype)) {
       throw new BadRequestException('Only images are allowed.');
     }
 
     const bucketName = env.BUCKET_NAME;
-    const fileName = file.originalname;
+    const fileName = `${uuidv4()}-${file.originalname}`;
     const uploadParams = {
       Bucket: bucketName,
       Key: `images/${fileName}`,
@@ -29,17 +33,25 @@ export class ImageService {
       ContentType: file.mimetype,
     };
 
-    await s3.send(new PutObjectCommand(uploadParams));
+    try {
+      await s3.send(new PutObjectCommand(uploadParams));
+    } catch (error) {
+      throw new BadRequestException('Failed to upload image.');
+    }
 
     const fileUrl = `https://${bucketName}.s3.${env.REGION}.backblazeb2.com/images/${encodeURIComponent(fileName)}`;
 
-    const savedImage = await this.prisma.image.create({
-      data: {
-        url: fileUrl,
-        path: fileName,
-      },
-    });
-
-    return savedImage;
+    try {
+      const savedImage = await this.prisma.image.create({
+        data: {
+          url: fileUrl,
+          path: fileName,
+          user_id,
+        },
+      });
+      return savedImage;
+    } catch (error) {
+      throw new BadRequestException('Failed to save image data.');
+    }
   }
 }
